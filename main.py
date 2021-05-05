e24c0976e33e4e1bef67cacc042b31d25e5de756
Rokas <rookas.rudzenskas@gmail.com>
2021-05-05 15:49:12 +0300

    Rokas

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ import cv2
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalcatface')
 
+webcam = cv2.VideoCapture(0)
 
+successful_frame_rate, frame = webcam.read()
+
+cv2.imshow("Why so seriour", frame)
+
+cv2.waitKey()
+
+# while True:
+#
+#
+
+# webcam.release()
+# cv2.destroyAllWubdiws()
 
 
 
