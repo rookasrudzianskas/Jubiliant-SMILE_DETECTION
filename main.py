a4c5f3490081b24a25ce7cbe2732108f5ea6d5c3
Rokas <rookas.rudzenskas@gmail.com>
2021-05-05 18:15:17 +0300

    Rokas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
+from random import randint
 
-face_detector = cv2.CascadeClassifier('haarcascade_frontalcatface')
+
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
 webcam = cv2.VideoCapture(0)
 
@@ -11,6 +13,13 @@ while True:
         break
 
     frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # faces = face_detector.detectMultiScale(frame_grayscale)
+
+    faces = face_detector.detectMultiScale(frame_grayscale)
+
+    print(faces)
+
     cv2.imshow("Why so serious?", frame_grayscale)
 
     cv2.waitKey(1)
